06319add7f5726b2b46cc79d4f4c8b459b2f218d
firepaperbird <thanvaquy1996@gmail.com>
2018-02-05 21:28:36 +0700

    user class

--- snkrshop/Models/user.cs
+++ snkrshop/Models/user.cs
@@ -5,9 +5,16 @@ using System.Web;
 
 namespace snkrshop.Models
 {
-    public class user
+    public class User
     {
         public string username { get; set; }
         public string password { get; set; }
+        public string email { get; set; }
+        public string fullname { get; set; }
+        public string adress { get; set; }
+        public string phone { get; set; }
+        public int gender { get; set; }
+        public int role { get; set; }
+        public DateTime regisDate { get; set; }
     }
 }
