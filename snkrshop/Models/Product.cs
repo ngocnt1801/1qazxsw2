a8499fca1c7a8e2ad6bdd19568b6ffbff6b26f9a
ngoc <ngocnt1801@gmail.com>
2018-02-05 22:16:20 +0700

    abc

---
+++ snkrshop/Models/Product.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace snkrshop.Models
+{
+    public class Product
+    {
+        public int productId { get; set; }
+    }
+}
