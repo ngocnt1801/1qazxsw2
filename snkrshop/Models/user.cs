1a812f6a9e38e51ae90d6b326cd059549192049f
firepaperbird <thanvaquy1996@gmail.com>
2018-02-05 19:54:58 +0700

    Up Quan's project

---
+++ snkrshop/Models/user.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace snkrshop.Models
+{
+    public class user
+    {
+        public string username { get; set; }
+        public string password { get; set; }
+    }
+}
