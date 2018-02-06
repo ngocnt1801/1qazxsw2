3feb1fce568f5f8feecbcd68bcd137350bcbf647
firepaperbird <thanvaquy1996@gmail.com>
2018-02-06 08:34:35 +0700

    Create order obj

---
+++ snkrshop/Models/order.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace snkrshop.Models
+{
+    public class order
+    {
+        public int OrderId { get; set; }
+        public DateTime OrderDate { get; set; }
+        public double TotalPrice { get; set; }
+        public int OrderStatus { get; set; }
+        public string UserId { get; set; }
+        public int GuestId { get; set; }
+        public string ApprovederId { get; set; }
+    }
+}
