c316e453138f511e30c8289b6ad601cc39c4520d
Nguyễn Thúy Ngọc <ngocnt1801@gmail.com>
2018-02-06 09:41:58 +0700

    Add Deal class in Models

---
+++ snkrshop/Models/Deal.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace snkrshop.Models
+{
+    public class Deal
+    {
+        public int Id { get; set; }
+        public string Content { get; set; }
+        public DateTime StartTime { get; set; }
+        public int Duration { get; set; }
+    }
+}
