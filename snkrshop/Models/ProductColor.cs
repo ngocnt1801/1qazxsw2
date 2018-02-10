e307e1e0fd8064c144196f9df76d02ddd2b4dcda
Nguyễn Thúy Ngọc <ngocnt1801@gmail.com>
2018-02-10 19:58:31 +0700

    Add 4 class in model for size and color

---
+++ snkrshop/Models/ProductColor.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace snkrshop.Models
+{
+    public class ProductColor
+    {
+        public int Id { get; set; }
+        public int ProductId { get; set; }
+        public int Color { get; set; }
+    }
+}
