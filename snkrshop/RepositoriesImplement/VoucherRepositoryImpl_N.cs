a916123f32323c2bbb6ca86792675576ffdebc7d
Nguyễn Thúy Ngọc <ngocnt1801@gmail.com>
2018-02-16 13:34:32 +0700

    add delete voucher

---
+++ snkrshop/RepositoriesImplement/VoucherRepositoryImpl_N.cs
@@ -0,0 +1,45 @@
+﻿using snkrshop.Repositories;
+using snkrshop.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace snkrshop.RepositoriesImplement
+{
+    public partial class VoucherRepositoryImpl : VoucherRepository
+    {
+        public bool DeleteVoucher(int voucherId)
+        {
+            SqlConnection cnn = DBUtils.GetConnection();
+            string sql = "DeleteVoucher";
+            SqlCommand cmd = new SqlCommand(sql, cnn);
+            cmd.Parameters.AddWithValue("@VoucherId", voucherId);
+            cmd.CommandType = CommandType.StoredProcedure;
+            int result;
+            try
+            {
+                if (cnn.State == ConnectionState.Closed)
+                {
+                    cnn.Open();
+                }
+                result = cmd.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+            finally
+            {
+                if (cnn.State == ConnectionState.Open)
+                {
+                    cnn.Close();
+                }
+
+            }
+            return result > 0;
+        }
+    }
+}
---
+++ snkrshop/ServicesImplement/VoucherServiceImpl_N.cs
@@ -0,0 +1,43 @@
+﻿using snkrshop.Repositories;
+using snkrshop.RepositoriesImplement;
+using snkrshop.Services;
+using snkrshop.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace snkrshop.ServicesImplement
+{
+    public partial class VoucherServiceImpl : VoucherService
+    {
+        const string FAIL = "fail";
+        const string SUCCESS = "success";
+        VoucherRepository voucherRepository;
+
+        public VoucherServiceImpl()
+        {
+            this.voucherRepository = new VoucherRepositoryImpl();
+        }
+
+        public string DeleteVoucher(int voucherId)
+        {
+            string result = FAIL;
+            try
+            {
+                if (voucherRepository.DeleteVoucher(voucherId))
+                {
+                    result = SUCCESS;
+                }
+            }
+            catch (Exception ex)
+            {
+                ex.LogExceptionToFile();
+                throw new Exception(ex.Message);
+
+
+            }
+            return result;
+        }
+    }
+}
