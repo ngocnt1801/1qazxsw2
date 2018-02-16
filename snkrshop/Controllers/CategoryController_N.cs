608cd53b6375df742bb3a7e97852c70edeb40b4e
Nguyễn Thúy Ngọc <ngocnt1801@gmail.com>
2018-02-16 11:22:45 +0700

    add delete category

---
+++ snkrshop/Controllers/CategoryController_N.cs
@@ -0,0 +1,22 @@
+﻿using snkrshop.Services;
+using snkrshop.ServicesImplement;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace snkrshop.Controllers
+{
+    public partial class CategoryController_N : ApiController
+    {
+        CategoryService categoryService = new CategoryServiceImpl();
+
+        [Route("category/delete")]
+        public string DeleteCategory(int categoryId)
+        {
+            return categoryService.DeleteCategory(categoryId);
+        }
+    }
+}
