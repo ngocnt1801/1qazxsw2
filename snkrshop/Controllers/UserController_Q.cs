9ef94500e349c427764e87a34f33af88497becad
Nguyễn Thúy Ngọc <ngocnt1801@gmail.com>
2018-03-06 09:31:35 +0700

    add test controller

--- snkrshop/Controllers/UserController_Q.cs
+++ snkrshop/Controllers/UserController_Q.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using snkrshop.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Http;
 using System.Web;
 using System.Web.Http;
 
@@ -10,10 +13,27 @@ namespace snkrshop.Controllers
     {
         [Route("user/login")]
         [HttpPost]
-        public int LoginUser(string username, string password)
+        [AcceptVerbs("POST")]
+        public int LoginUser(LoginForm loginForm)
         {
-            return userService.LoginUser(username, password);
+            return userService.LoginUser(loginForm.Username, loginForm.Password);
         }
+
+        [Route("user/test")]
+        [HttpPost]
+        [AcceptVerbs("POST")]
+        public HttpResponseMessage Test(String id)
+        {
+            return Request.CreateResponse<String>(HttpStatusCode.OK,"thanh cong " + id);
+        }
+
+        [Route("user/abc")]
+        [HttpGet]
+        public HttpResponseMessage Test2(String id)
+        {
+            return Request.CreateResponse<String>(HttpStatusCode.OK, "thanh cong " + id);
+        }
+
         [Route("user/set/role")]
         [HttpPost]
         public string SetRole(string username, int role)
